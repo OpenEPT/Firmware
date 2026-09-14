34bac4ada5ff27daf995ae566fa228c32c95ecfb
hturkmanovic <hturkmanovic@asensus-consultant.com>
2026-09-14 14:12:19 +0200

    cmparse: always NUL-terminate parsed argument values (fixes wrong prescaler/period when handler reuses value buffer)

--- Source/ADFirmware/CM7/Core/Middlewares/Services/Control/CMParse/cmparse.c
+++ Source/ADFirmware/CM7/Core/Middlewares/Services/Control/CMParse/cmparse.c
@@ -239,7 +239,7 @@ char*					CMPARSE_GetArgParameters(char* argBuffer, uint32_t* argBufferSize, cmp
 	{
 		tmpArgBufferSizeProcessed += 1;
 		tmpArgBuffPtr += 1;
-		while(*tmpArgBuffPtr != '"' && *tmpArgBuffPtr != 0 && tmpArgBufferSizeProcessed < *argBufferSize)
+		while(*tmpArgBuffPtr != '"' && *tmpArgBuffPtr != 0 && tmpArgBufferSizeProcessed < *argBufferSize && value->size < (CMPARSE_MAX_VALUE_SIZE - 1))
 		{
 			value->value[value->size] = *tmpArgBuffPtr;
 			value->size += 1;
@@ -251,9 +251,10 @@ char*					CMPARSE_GetArgParameters(char* argBuffer, uint32_t* argBufferSize, cmp
 			tmpArgBufferSizeProcessed += 1;
 			tmpArgBuffPtr += 1;
 		}
+		value->value[value->size] = 0;
 		return tmpArgBuffPtr;
 	}
-	while(*tmpArgBuffPtr != ' ' && *tmpArgBuffPtr != 0 && tmpArgBufferSizeProcessed < *argBufferSize)
+	while(*tmpArgBuffPtr != ' ' && *tmpArgBuffPtr != 0 && tmpArgBufferSizeProcessed < *argBufferSize && value->size < (CMPARSE_MAX_VALUE_SIZE - 1))
 	{
 		value->value[value->size] = *tmpArgBuffPtr;
 		value->size += 1;
@@ -263,8 +264,8 @@ char*					CMPARSE_GetArgParameters(char* argBuffer, uint32_t* argBufferSize, cmp
 	while((value->size > 0) && ((value->value[value->size - 1] == '\r') || (value->value[value->size - 1] == '\n')))
 	{
 		value->size -= 1;
-		value->value[value->size] = 0;
 	}
+	value->value[value->size] = 0;
 	return tmpArgBuffPtr;
 
 }
@@ -327,6 +328,7 @@ cmparse_status_t		CMPARSE_GetArgValue(const char* argBuffer, uint32_t argBufferS
 		{
 			value->size = localValue.size;
 			memcpy(value->value, localValue.value, value->size);
+			value->value[value->size] = 0;
 			return CMPARSE_STATUS_OK;
 		}
 	}while(unprocessedArgBufferPtr != NULL);
